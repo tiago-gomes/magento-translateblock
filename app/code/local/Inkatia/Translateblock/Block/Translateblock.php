071d62c1ec602a2881f6f7d81d4b0ffde7d7cfd0
Tiago Gomes <geone85@live.com.pt>
2013-11-23 01:09:25 +0000

    first commit

---
+++ app/code/local/Inkatia/Translateblock/Block/Translateblock.php
@@ -0,0 +1,5 @@
+<?php
+
+class Inkatia_Translateblock_Block_Translateblock extends Mage_Core_Block_Template{
+
+}
---
+++ app/code/local/Inkatia/Translateblock/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class Inkatia_Translateblock_Helper_Data extends Mage_Core_Helper_Abstract{
+
+
+}
